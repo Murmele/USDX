b61e714a75fb2b3788e5121c9903a6e5644ff6fd
Martin Marmsoler <martin.marmsoler@gmail.com>
2020-03-03 19:19:44 +0100

    update

---
+++ Flatpak/installFlatpak.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+
+#flatpak-builder --repo=meinRepo2 --force-clean UltraStarDeluxe eu.usdx.UltraStarDeluxe.json
+#flatpak remote-add meinRepo2 meinRepo2 --no-gpg-verify
+#flatpak install meinRepo2 eu.usdx.UltraStarDeluxe
